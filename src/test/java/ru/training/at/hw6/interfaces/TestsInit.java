3dd90f9212312c5606911dfc1b2a2697f44a3764
iRezograf <iRezograf@gmail.com>
2021-05-29 09:23:02 +0300

    JDI test started

---
+++ src/test/java/ru/training/at/hw6/interfaces/TestsInit.java
@@ -0,0 +1,16 @@
+package ru.training.at.hw6.interfaces;
+
+public class TestsInit {
+    /*
+    @BeforeSuite(alwaysRun = true)
+    public void beforeSuite() {
+        PageFactory.initElements(SiteJdi.class);
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void afterSuite() {
+        WebDriverUtils.killAllSeleniumDrivers();
+    }
+
+     */
+}
